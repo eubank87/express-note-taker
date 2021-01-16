5d54c2b9e986965dc3b252f509c063c4095b0ae1
eubank87 <eubank.ashley@gmail.com>
2021-01-16 12:47:08 -0800

    installed uuid npm pkg

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const path = require("path");
+const { v4: uuidv4} = require("uuid");
+uuidv4();
 
 // Sets up the express app
 // ========================================================
@@ -15,25 +17,39 @@ app.use(express.static("public"));
 
 const notes = [];
 
+// route to home page
 app.get("/", (req, res)=>{
     res.sendFile(path.join(__dirname, "index.html"))
 })
 
+// route to notes page
 app.get("/notes", (req, res)=>{
     res.sendFile(path.join(__dirname, "notes.html"))
 })
 
-
+// route to notes json page
 app.get("/api/notes", (req, res)=>{
     res.json(notes);
 })
 
+// receives new note to save on the request body, returns note to client
 app.post("/api/notes", (req, res)=>{
     const newNote = req.body;
     notes.push(newNote)
     console.log(newNote)
     console.log("Notes array", notes);
-    re.json(newNote);
+    res.json(newNote);
+})
+
+
+
+// receive query parameter w/id of note to delete
+app.delete("api/notes/:id", (req, res)=>{
+    const userIndex = getUserIndex(req.params.id)
+    if(userIndex === -1) return res.status(404).json({})
+
+    notes.splice(userIndex, 1)
+    res.json(notes)
 })
 
 
